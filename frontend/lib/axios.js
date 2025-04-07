5166bc903a156395ea1db334d1de3afab0572bea
Thoangne <Nguythoang2003@gmail.com>
2025-04-08 01:11:19 +0700

    Update axios.js

--- frontend/lib/axios.js
+++ frontend/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "http://localhost:5000/api"
+      ? "VITE_SOCKET_BASE_URL/api"
       : "/api",
   withCredentials: true,
   headers: {
