ec251abec880d92b3910ed14849bf7fff7058e2a
Thoangne <Nguythoang2003@gmail.com>
2025-04-18 15:52:39 +0700

    update

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -2,16 +2,17 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const generateToken = (user,res) => {
-    const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: "1d"});
+export const generateToken = (user, res) => {
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
 
-    res.cookie("jwt", token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV !== "production",
-        maxAge: 24 * 60 * 60 * 1000, // 1 day
-        sameSite: "strict",
-    });
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "production",
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
+    sameSite: "strict",
+  });
 
-    return token;
-
-}
+  return token;
+};
